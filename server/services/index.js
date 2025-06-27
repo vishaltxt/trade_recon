7474f7b552004a7e1a179b3d34331d2fd05e05b6
vishaltxt <9318487019a@gmail.com>
2025-06-27 16:28:48 +0530

    buy xtsApi

--- server/services/index.js
+++ server/services/index.js
@@ -1,12 +1,5 @@
-// index.js
-const getXtsToken = require('./xtsAuth');
-const placeOrder = require('./xtsTrade');
-const connectSpi = require('./xtsSpi');
+var XTSRest = require("./interactiveRestAPI");
+var XTSEmitter = require("./interactiveSocket");
 
-const run = async () => {
-  const token = await getXtsToken();
-  connectSpi(token);
-  await placeOrder(token);
-};
-
-run();
+module.exports.Interactive = XTSRest;
+module.exports.WS = XTSEmitter;
